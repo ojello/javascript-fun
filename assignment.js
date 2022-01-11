c9c3deeba3c70382a04c98af9f4a4bd240ccd5e0
Kist <kbear.swe@gmail.com>
2022-01-11 12:10:11 -0600

    assignments

---
+++ assignment.js
@@ -0,0 +1,31 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+
+const country = 'USA';
+const continent = 'North America';
+const population = 200000;
+
+console.log(population)
+console.log(continent)
+console.log(country)
+
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+
+const isIsland = False;
+let language;
+console.log(isIsland)
+console.log(population)
+console.log(country)
+// language is undefined
+console.log(language)
+
+
