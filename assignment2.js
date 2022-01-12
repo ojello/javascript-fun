7cd01257dd86d52fd9af03777ef42a8f3ebc0bb8
Kist <kbear.swe@gmail.com>
2022-01-11 19:06:26 -0600

    update

--- assignment2.js
+++ assignment2.js
@@ -41,7 +41,7 @@ const angora = percentageOfWorld1("Angora", 1441)
 const china = percentageOfWorld1("China", 450)
 
 
-// Function Expression - React version ** Preferred Method
+// Function Expression - React version ** Preferred Method ***
 const percentageOfWorld2 = function(country, population){
     let percentage = population/7900 * 100;
     let cPop = `${country} has ${population} million people so it's about ${percentage}% of the world population`
@@ -52,8 +52,19 @@ const percentageOfWorld2 = function(country, population){
 const taiwan = percentageOfWorld2("taiwan", 400);
 console.log(taiwan);
 
+const japan = percentageOfWorld2("japan", 200);
+console.log(japan);
 
 
-
+// Arrow Functions -- preferred **
+// 1. Recreate the last assignment, but this time create an arrow function called
+// 'percentageOfWorld3
+// 1. Create a function called 'describePopulation'. Use the function type you
+// like the most. This function takes in two arguments: 'country' and
+// 'population', and returns a string like this: 'China has 1441 million people,
+// which is about 18.2% of the world.'
+// 2. To calculate the percentage, 'describePopulation' call the
+// 'percentageOfWorld1' you created earlier
+// 3. Call 'describePopulation' with data for 3 countries of your choice
 
 
