26567b013fb7311a77e777a399a33a209346ddcb
Kist <kbear.swe@gmail.com>
2022-01-11 18:10:51 -0600

    update

--- assignment.js
+++ assignment.js
@@ -157,4 +157,16 @@ switch (langOne) {
 }
 
 
+// ternary operator example
+function getFee(isSenior) {
+    return (isSenior ? '5.00' : '25.00');
+}
 
+console.log(getFee(true));
+// expected output: 5.00
+
+console.log(getFee(false));
+// expected output: 25.00
+
+console.log(getFee(null));
+// expected output: 26.00
