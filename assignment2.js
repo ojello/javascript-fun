cf023563620d42851ea335e4f5fa648ba87377a8
Kist <kbear.swe@gmail.com>
2022-01-12 12:49:38 -0600

    update

--- assignment2.js
+++ assignment2.js
@@ -89,3 +89,18 @@ const describePopulation = function(country, population) {
 describePopulation("asia", 1200);
 
 
+// LECTURE: Introduction to Arrays
+// 1. Create an array containing 4 population values of 4  countries of your choice. You may use the values you have been using previously. Store this array into a variable called 'populations'
+// 2. Log to the console whether the array has 4 elements or not(true or false)
+// 3. Create an array called 'percentages' containing the percentages of the world population for these 4 population values. Use the function 'percentageOfWorld1' that you created earlier to compute the 4 percentage values
+
+
+
+let populations = [4400, 1200, 134,  24];
+
+populations === 4;
+
+let percentages = [percentageOfWorld5(populations[0]),percentageOfWorld5(populations[1]), percentageOfWorld5(populations[2]), percentageOfWorld5(populations[3])];
+
+console.log(percentages)
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,9 @@
 
-const taiwan = percentageOfWorld2("taiwan", 400);
-console.log(taiwan);
+function percentageOfWorld5(population) 
+{
+    return (population/7900) * 100;
+}
+
+let populations = [4400, 1200, 134,  24];
+
+let percentages = [percentageOfWorld5(populations[0]),percentageOfWorld5(populations[1]), percentageOfWorld5(populations[2]), percentageOfWorld5(populations[3])];
